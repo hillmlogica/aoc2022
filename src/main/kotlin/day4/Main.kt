3f4e46864472cdc47dbe27fa4bb8229bea2b9b7b
Mike Hill <mikewhill@gmail.com>
2024-01-11 18:42:26 +0000

    day 4 complete

--- src/main/kotlin/day4/Main.kt
+++ src/main/kotlin/day4/Main.kt
@@ -10,9 +10,35 @@ fun main() {
 }
 
 fun answer2(puzzleInput: String): Int {
-    return 0
+    return parseRanges(puzzleInput)
+            .map { overlaps(it.first, it.second) }
+            .map { if (it) 1 else 0 }
+            .sum()
+
 }
 
 fun answer(puzzleInput: String): Int {
-    return 0
+    return parseRanges(puzzleInput)
+            .map { isFullyContained(it.first, it.second) }
+            .map { if (it) 1 else 0 }
+            .sum()
+}
+
+private fun parseRanges(puzzleInput: String) = puzzleInput.lines()
+        .map {
+            val ranges = it.split(",")
+            Pair(range(ranges[0]), range(ranges[1]))
+        }
+
+fun isFullyContained(left: IntRange, right: IntRange): Boolean {
+    return (right.contains(left.first) && right.contains(left.last)) ||
+            (left.contains(right.first) && left.contains(right.last))
+}
+fun overlaps(left: IntRange, right: IntRange): Boolean {
+    return right.contains(left.first) || right.contains(left.last) || left.contains(right.first) || left.contains(right.last)
+}
+
+fun range(rangeAsString: String): IntRange {
+    val parts = rangeAsString.split("-")
+    return IntRange(parts[0].toInt(), parts[1].toInt())
 }
--- src/test/kotlin/day4/Day4Test.kt
+++ src/test/kotlin/day4/Day4Test.kt
@@ -18,6 +18,6 @@ class Day4Test {
 
     @Test
     fun `check part 2 example`() {
-        Assertions.assertThat(answer2(exampleInput)).isEqualTo(0)
+        Assertions.assertThat(answer2(exampleInput)).isEqualTo(4)
     }
 }
