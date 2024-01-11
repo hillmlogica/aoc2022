c32b12910593371f874cfb952e87461df9a9d318
Mike Hill <mikewhill@gmail.com>
2024-01-11 18:00:15 +0000

    day 3 prep

---
+++ src/main/kotlin/day3/Main.kt
@@ -0,0 +1,18 @@
+package day3
+
+import java.io.File
+
+fun main() {
+    val puzzleInput = File("day3/puzzleInput.txt").readText()
+    println("answer1: " + answer(puzzleInput))
+    println("answer2: " + answer2(puzzleInput))
+
+}
+
+fun answer2(puzzleInput: String): Int {
+    return 0
+}
+
+fun answer(puzzleInput: String): Int {
+    return 0
+}
---
+++ src/test/kotlin/day3/Day2Test.kt
@@ -0,0 +1,23 @@
+package day3
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day3Test {
+    val exampleInput = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+
+    @Test
+    fun `check part 1 example`() {
+        Assertions.assertThat(answer(exampleInput)).isEqualTo(157)
+    }
+
+    @Test
+    fun `check part 2 example`() {
+        Assertions.assertThat(answer2(exampleInput)).isEqualTo(0)
+    }
+}
