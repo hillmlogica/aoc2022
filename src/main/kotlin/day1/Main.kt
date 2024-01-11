2c96e254570c10ab7b6ca8bbb4d3550ec9e9651c
Mike Hill <mikewhill@gmail.com>
2024-01-11 16:50:48 +0000

    day 1 part 1 complete

--- src/main/kotlin/day1/Main.kt
+++ src/main/kotlin/day1/Main.kt
@@ -4,10 +4,20 @@ import java.io.File
 
 fun main() {
     val puzzleInput = File("day1/puzzleInput.txt").readText()
-    println(puzzleInput)
+    println(answer(puzzleInput))
 
 }
 
 fun answer(puzzleInput: String): Int {
-    return 0
+    val groups = puzzleInput.lines().fold(listOf(listOf())) { acc: List<List<Int>>, s: String ->
+        val lists: List<List<Int>> = if (s.isBlank()) {
+            acc + listOf(listOf())
+        } else {
+            val untouched = acc.subList(0, acc.size - 1)
+            val extended = listOf(acc.last() + s.toInt())
+            untouched + extended
+        }
+        lists
+    }
+    return groups.map { it.sum() }.max()
 }
--- src/test/kotlin/Day1Test.kt
+++ src/test/kotlin/Day1Test.kt
@@ -3,8 +3,23 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day1Test {
+    val exampleInput = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
     @Test
     fun `test something`() {
-        Assertions.assertThat(answer("sss")).isEqualTo(0)
+        Assertions.assertThat(answer(exampleInput)).isEqualTo(24000)
     }
 }
