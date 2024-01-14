f9cfeea49db3b97e4990fc100b4426c3bf7c6dff
Mike Hill <mikewhill@gmail.com>
2024-01-14 07:10:39 +0000

    day 9 complete

--- src/main/kotlin/day9/Main.kt
+++ src/main/kotlin/day9/Main.kt
@@ -11,31 +11,68 @@ fun main() {
 }
 
 fun answer2(puzzleInput: String): Int {
-    return 0
+    var worm = makeWormOfSize(10)
+    return applyInstructionsTo(worm, parseInstructions(puzzleInput))
+}
+
+fun makeWormOfSize(size: Int): Worm {
+    var worm = Worm(Coord(0, 0), Coord(0,0))
+    (2 until size).forEach {
+        worm = Worm(Coord(0, 0), worm)
+    }
+    return worm
 }
 
 fun answer(puzzleInput: String): Int {
+    var worm = makeWormOfSize(2)
+    return applyInstructionsTo(worm, parseInstructions(puzzleInput))
+}
+
+private fun applyInstructionsTo(worm: Worm, instructions: Sequence<Direction>): Int {
+    var worm1 = worm
     val tails = mutableSetOf<Coord>()
-    var worm = Worm(Coord(0, 0), Coord(0,0))
-    tails.add(worm.tail)
-    puzzleInput.lines().forEach {
+    tails.add(worm1.tailCoord())
+    instructions.forEach {
+        worm1 = worm1.move(it)
+        tails.add(worm1.tailCoord())
+    }
+    return tails.size
+}
+
+fun parseInstructions(puzzleInput: String): Sequence<Direction> {
+    val lines = puzzleInput.lines()
+    return lines.flatMap {
         val direction = DIRECTIONS_BY_CHAR[it[0]]!!
         val (quantityAsString) = "\\w (\\d+)".toRegex().matchEntire(it)!!.destructured
         val quantity = quantityAsString.toInt()
-        (0 until quantity).forEach{
-            worm = worm.move(direction)
-            tails.add(worm.tail)
-        }
-    }
-    return tails.size
+        List(quantity) { direction }
+    }.asSequence()
 }
 
 val DIRECTIONS_BY_CHAR = Direction.entries.associateBy { it.c }
 enum class Direction(val c: Char) { Right('R'), Left('L'), Up('U'), Down('D') }
 
-data class Coord(val x: Int, val y: Int)
+data class Coord(val x: Int, val y: Int) : Tail {
+    override fun x(): Int = x
+    override fun y(): Int = y
+    override fun move(moveX: Int, moveY: Int): Tail = Coord(x + moveX, y + moveY)
+    override fun tailCoord(): Coord = this
+    override fun coords(): List<Coord> {
+        return listOf(this)
+    }
+}
 
-data class Worm(val head: Coord, val tail: Coord) {
+interface Tail {
+    fun x(): Int
+    fun y(): Int
+
+    fun move(moveX: Int, moveY: Int): Tail
+
+    fun tailCoord(): Coord
+    fun coords(): List<Coord>
+}
+
+data class Worm(val head: Coord, val tail: Tail) : Tail {
     fun move(direction: Direction) : Worm {
         return when(direction) {
             Direction.Right -> moveRight()
@@ -58,15 +95,29 @@ data class Worm(val head: Coord, val tail: Coord) {
         return move(0, 1)
     }
 
-    private fun move(moveX: Int, moveY: Int): Worm {
+    override fun move(moveX: Int, moveY: Int): Worm {
         val newHead = Coord(head.x + moveX, head.y + moveY)
-        val newTail = if (abs(newHead.x - tail.x) == 2) {
-            Coord(tail.x + moveX, if(newHead.y == tail.y) tail.y else head.y)
-        } else if (abs(newHead.y - tail.y) == 2) {
-            Coord(if(newHead.x == tail.x) tail.x else head.x, tail.y + moveY)
+        val tailXMove: Int
+        val tailYMove: Int
+        if ((abs(newHead.x - tail.x()) == 2 && newHead.y != tail.y()) || (abs(newHead.y - tail.y()) == 2 && newHead.x != tail.x())) {
+            tailXMove = if (newHead.x - tail.x() == 2) 1 else if (newHead.x - tail.x() == -2) -1 else newHead.x - tail.x()
+            tailYMove = if (newHead.y - tail.y() == 2) 1 else if (newHead.y - tail.y() == -2) -1 else newHead.y - tail.y()
+        } else if ((abs(newHead.x - tail.x()) == 2) || (abs(newHead.y - tail.y()) == 2)) {
+            tailXMove = if (newHead.x - tail.x() == 2) 1 else if (newHead.x - tail.x() == -2) -1 else 0
+            tailYMove = if (newHead.y - tail.y() == 2) 1 else if (newHead.y - tail.y() == -2) -1 else 0
         } else {
-            tail
+            tailXMove = 0
+            tailYMove = 0
         }
-        return Worm(newHead, newTail)
+        return Worm(newHead, tail.move(tailXMove, tailYMove))
     }
+
+    override fun tailCoord(): Coord = tail.tailCoord()
+    override fun coords(): List<Coord> {
+        return listOf(head) + tail.coords()
+    }
+
+    override fun x(): Int = head.x
+
+    override fun y(): Int = head.y
 }
--- src/test/kotlin/day9/Day9Test.kt
+++ src/test/kotlin/day9/Day9Test.kt
@@ -13,155 +13,238 @@ D 1
 L 5
 R 2"""
 
+    val example2Input = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"""
+
     @Test
     fun `check part 1 example`() {
         Assertions.assertThat(answer(exampleInput)).isEqualTo(13)
     }
+
     @Test
     fun `check part 2 example`() {
-        Assertions.assertThat(answer2(exampleInput)).isEqualTo(0)
+        Assertions.assertThat(answer2(example2Input)).isEqualTo(36)
     }
 
     @Test
     fun `move right when head over tail`() {
-        val actual =  Worm(Coord(0,0), Coord(0,0)).moveRight()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, 0), Coord(0,0)))
+        val actual = Worm(Coord(0, 0), Coord(0, 0)).moveRight()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, 0), Coord(0, 0)))
     }
 
     @Test
     fun `move left when head over tail`() {
-        val actual =  Worm(Coord(0,0), Coord(0,0)).moveLeft()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, 0), Coord(0,0)))
+        val actual = Worm(Coord(0, 0), Coord(0, 0)).moveLeft()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, 0), Coord(0, 0)))
     }
 
     @Test
     fun `move up when head over tail`() {
-        val actual =  Worm(Coord(0,0), Coord(0,0)).moveUp()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, -1), Coord(0,0)))
+        val actual = Worm(Coord(0, 0), Coord(0, 0)).moveUp()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, -1), Coord(0, 0)))
     }
 
     @Test
     fun `move right when head on rhs of tail`() {
-        val actual =  Worm(Coord(1,0), Coord(0,0)).moveRight()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(2, 0), Coord(1,0)))
+        val actual = Worm(Coord(1, 0), Coord(0, 0)).moveRight()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(2, 0), Coord(1, 0)))
     }
 
     @Test
     fun `move left when head on rhs of tail`() {
-        val actual =  Worm(Coord(1,0), Coord(0,0)).moveLeft()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 0), Coord(0,0)))
+        val actual = Worm(Coord(1, 0), Coord(0, 0)).moveLeft()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 0), Coord(0, 0)))
     }
+
     @Test
     fun `move up when head on rhs of tail`() {
-        val actual =  Worm(Coord(1,0), Coord(0,0)).moveUp()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, -1), Coord(0,0)))
+        val actual = Worm(Coord(1, 0), Coord(0, 0)).moveUp()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, -1), Coord(0, 0)))
+    }
+    @Test
+    fun `move diag up when head on rhs of tail`() {
+        val actual = Worm(Coord(1, 0), Coord(0, 0)).move(1, -1)
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(2, -1), Coord(1, -1)))
     }
 
     @Test
     fun `move right when head on lhs of tail`() {
-        val actual =  Worm(Coord(-1,0), Coord(0,0)).moveRight()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 0), Coord(0,0)))
+        val actual = Worm(Coord(-1, 0), Coord(0, 0)).moveRight()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 0), Coord(0, 0)))
     }
+
     @Test
     fun `move left when head on lhs of tail`() {
-        val actual =  Worm(Coord(-1,0), Coord(0,0)).moveLeft()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-2, 0), Coord(-1,0)))
+        val actual = Worm(Coord(-1, 0), Coord(0, 0)).moveLeft()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-2, 0), Coord(-1, 0)))
     }
+
     @Test
     fun `move up when head on lhs of tail`() {
-        val actual =  Worm(Coord(-1,0), Coord(0,0)).moveUp()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, -1), Coord(0,0)))
+        val actual = Worm(Coord(-1, 0), Coord(0, 0)).moveUp()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, -1), Coord(0, 0)))
     }
+
     @Test
     fun `move right when head directly above tail`() {
-        val actual =  Worm(Coord(0,-1), Coord(0,0)).moveRight()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, -1), Coord(0,0)))
+        val actual = Worm(Coord(0, -1), Coord(0, 0)).moveRight()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, -1), Coord(0, 0)))
     }
+
     @Test
     fun `move left when head directly above tail`() {
-        val actual =  Worm(Coord(0,-1), Coord(0,0)).moveLeft()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, -1), Coord(0,0)))
+        val actual = Worm(Coord(0, -1), Coord(0, 0)).moveLeft()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, -1), Coord(0, 0)))
     }
+
     @Test
     fun `move up when head directly above tail`() {
-        val actual =  Worm(Coord(0,-1), Coord(0,0)).moveUp()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, -2), Coord(0,-1)))
+        val actual = Worm(Coord(0, -1), Coord(0, 0)).moveUp()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, -2), Coord(0, -1)))
     }
 
     @Test
     fun `move right when head below tail`() {
-        val actual =  Worm(Coord(0,1), Coord(0,0)).moveRight()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, 1), Coord(0,0)))
+        val actual = Worm(Coord(0, 1), Coord(0, 0)).moveRight()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, 1), Coord(0, 0)))
     }
+
     @Test
     fun `move left when head below tail`() {
-        val actual =  Worm(Coord(0,1), Coord(0,0)).moveLeft()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, 1), Coord(0,0)))
+        val actual = Worm(Coord(0, 1), Coord(0, 0)).moveLeft()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, 1), Coord(0, 0)))
     }
+
     @Test
     fun `move up when head below tail`() {
-        val actual =  Worm(Coord(0,1), Coord(0,0)).moveUp()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 0), Coord(0,0)))
+        val actual = Worm(Coord(0, 1), Coord(0, 0)).moveUp()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 0), Coord(0, 0)))
     }
+
     @Test
     fun `move right when head up left from tail`() {
-        val actual =  Worm(Coord(0,0), Coord(1,1)).moveRight()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, 0), Coord(1,1)))
+        val actual = Worm(Coord(0, 0), Coord(1, 1)).moveRight()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, 0), Coord(1, 1)))
     }
+
     @Test
     fun `move left when head up left from tail`() {
-        val actual =  Worm(Coord(0,0), Coord(1,1)).moveLeft()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, 0), Coord(0,0)))
+        val actual = Worm(Coord(0, 0), Coord(1, 1)).moveLeft()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, 0), Coord(0, 0)))
     }
+
     @Test
     fun `move up when head up left from tail`() {
-        val actual =  Worm(Coord(0,0), Coord(1,1)).moveUp()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, -1), Coord(0,0)))
+        val actual = Worm(Coord(0, 0), Coord(1, 1)).moveUp()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, -1), Coord(0, 0)))
     }
+
     @Test
     fun `move right when head down left from tail`() {
-        val actual =  Worm(Coord(0,1), Coord(1,0)).moveRight()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, 1), Coord(1,0)))
+        val actual = Worm(Coord(0, 1), Coord(1, 0)).moveRight()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, 1), Coord(1, 0)))
     }
+
     @Test
     fun `move left when head down left from tail`() {
-        val actual =  Worm(Coord(0,1), Coord(1,0)).moveLeft()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, 1), Coord(0,1)))
+        val actual = Worm(Coord(0, 1), Coord(1, 0)).moveLeft()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(-1, 1), Coord(0, 1)))
     }
+
     @Test
     fun `move up when head down left from tail`() {
-        val actual =  Worm(Coord(0,1), Coord(1,0)).moveUp()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 0), Coord(1,0)))
+        val actual = Worm(Coord(0, 1), Coord(1, 0)).moveUp()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 0), Coord(1, 0)))
     }
+
     @Test
     fun `move right when head up right from tail`() {
-        val actual =  Worm(Coord(1,0), Coord(0,1)).moveRight()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(2, 0), Coord(1,0)))
+        val actual = Worm(Coord(1, 0), Coord(0, 1)).moveRight()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(2, 0), Coord(1, 0)))
     }
+
     @Test
     fun `move left when head up right from tail`() {
-        val actual =  Worm(Coord(1,0), Coord(0,1)).moveLeft()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 0), Coord(0,1)))
+        val actual = Worm(Coord(1, 0), Coord(0, 1)).moveLeft()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 0), Coord(0, 1)))
     }
+
     @Test
     fun `move up when head up right from tail`() {
-        val actual =  Worm(Coord(1,0), Coord(0,1)).moveUp()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, -1), Coord(1,0)))
+        val actual = Worm(Coord(1, 0), Coord(0, 1)).moveUp()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, -1), Coord(1, 0)))
     }
+
     @Test
     fun `move right when head down right from tail`() {
-        val actual =  Worm(Coord(1,1), Coord(0,0)).moveRight()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(2, 1), Coord(1,1)))
+        val actual = Worm(Coord(1, 1), Coord(0, 0)).moveRight()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(2, 1), Coord(1, 1)))
     }
+
     @Test
     fun `move left when head down right from tail`() {
-        val actual =  Worm(Coord(1,1), Coord(0,0)).moveLeft()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 1), Coord(0,0)))
+        val actual = Worm(Coord(1, 1), Coord(0, 0)).moveLeft()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(0, 1), Coord(0, 0)))
     }
+
     @Test
     fun `move up when head down right from tail`() {
-        val actual =  Worm(Coord(1,1), Coord(0,0)).moveUp()
-        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, 0), Coord(0,0)))
+        val actual = Worm(Coord(1, 1), Coord(0, 0)).moveUp()
+        Assertions.assertThat(actual).isEqualTo(Worm(Coord(1, 0), Coord(0, 0)))
+    }
+
+    @Test
+    fun `debug example 2`() {
+        var worm = makeWormOfSize(10)
+        val tailCoords = mutableSetOf<Coord>()
+        tailCoords.add(worm.tailCoord())
+        worm = move(worm, tailCoords, 5, Direction.Right)
+        println("\n***** after 5 RIGHT ****")
+        print(worm)
+        Assertions.assertThat(tailCoords).containsExactlyInAnyOrder(Coord(0, 0))
+        worm = move(worm, tailCoords, 8, Direction.Up)
+        println("\n***** after 8 UP ****")
+        print(worm)
+        Assertions.assertThat(tailCoords).containsExactlyInAnyOrder(Coord(0, 0))
+        worm = move(worm, tailCoords, 8, Direction.Left)
+        println("\n***** after 8 LEFT ****")
+        print(worm)
+        Assertions.assertThat(tailCoords).containsExactlyInAnyOrder(Coord(0, 0), Coord(1, -1), Coord(2, -2), Coord(1, -3))
+    }
+
+    fun print(worm: Worm) {
+
+        val wormCoords = worm.coords()
+        val lines = (-20..20).map { y ->
+            String(
+                (-20..20).map { x ->
+                    if (wormCoords.contains(Coord(x, y))) {
+                        val index = wormCoords.indexOf(Coord(x, y))
+                        val char =
+                            if (index == 0) 'H' else if (index == wormCoords.size - 1) 'T' else index.digitToChar()
+                        char
+                    } else {
+                        '.'
+                    }
+                }.toCharArray()
+            )
+        }
+        lines.forEach{println(it)}
+    }
+
+    private fun move(worm: Worm, tailCoords: MutableSet<Coord>, howMany: Int, direction: Direction): Worm {
+        var worm1 = worm
+        (0 until howMany).forEach {
+            worm1 = worm1.move(direction)
+            tailCoords.add(worm1.tailCoord())
+        }
+        return worm1
     }
 
 }
