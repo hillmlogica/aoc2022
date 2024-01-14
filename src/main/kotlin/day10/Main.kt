708913018f595ae3d124ecefe7cf5becca254666
Mike Hill <mikewhill@gmail.com>
2024-01-14 07:46:27 +0000

    day 10 part 1

--- src/main/kotlin/day10/Main.kt
+++ src/main/kotlin/day10/Main.kt
@@ -15,5 +15,20 @@ fun answer2(puzzleInput: String): Int {
 }
 
 fun answer(puzzleInput: String): Int {
-    return 0
+    return puzzleInput.lines().map {
+        if (it == "noop") Pair(1, 0) else {
+            val (quantityAsString) = "addx (-?\\d+)".toRegex().matchEntire(it)!!.destructured
+            Pair(2, quantityAsString.toInt())
+        }
+    }.runningFold(Cycle(0, 1, 1, 1)) {
+        acc: Cycle, operation: Pair<Int, Int> -> Cycle(acc.to, acc.to + operation.first, acc.endingX, acc.endingX + operation.second)
+    }.filter {
+        (it.from until it.to).any{cycle -> (cycle - 20) % 40 == 0}
+    }.mapIndexed({ index: Int, cycle: Cycle ->
+        val cycleId = 20 + (40*index)
+        val strength = cycleId * cycle.startingX
+        strength
+    }).sum()
 }
+
+data class Cycle(val from: Int, val to: Int, val startingX: Int, val endingX: Int)
