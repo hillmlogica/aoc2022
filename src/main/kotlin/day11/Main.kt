9ba5c5c36fd34f6832d8533a13b5e32d3aaea67d
Mike Hill <mikewhill@gmail.com>
2024-01-14 14:00:16 +0000

    day 11 part 1 prep

---
+++ src/main/kotlin/day11/Main.kt
@@ -0,0 +1,19 @@
+package day11
+
+import java.io.File
+import kotlin.math.abs
+
+fun main() {
+    val puzzleInput = File("1/puzzleInput.txt").readText()
+    println("answer1: " + answer(puzzleInput))
+    println("answer2: " + answer2(puzzleInput))
+
+}
+
+fun answer2(puzzleInput: String): Int {
+    return 0
+}
+
+fun answer(puzzleInput: String): Int {
+    return 0
+}
