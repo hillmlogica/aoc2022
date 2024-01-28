25dbd5f055a9353039b8adf5787cc8abb25dc352
Mike Hill <mikewhill@gmail.com>
2024-01-28 13:51:54 +0000

    mike - day 12 part 1

--- src/main/kotlin/day12/Main.kt
+++ src/main/kotlin/day12/Main.kt
@@ -17,5 +17,62 @@ fun answer2(puzzleInput: String): Long {
 }
 
 fun answer(puzzleInput: String): Int {
-    return 0
+    val g = Grid(puzzleInput.lines())
+    val shortestPaths = mutableMapOf<Coord, Int>()
+    val q = mutableListOf(g.start())
+    (0 until g.height()).map { y ->
+        (0 until g.width()).map { x ->
+            shortestPaths[Coord(x,y)] = Int.MAX_VALUE
+            q.add(Coord(x,y))
+        }
+    }
+    shortestPaths[g.start()] = 0
+    while (q.isNotEmpty()) {
+        val e = q.minBy { shortestPaths[it]!! }
+        q.remove(e)
+        val neighbours = g.neighbours(e).filter { q.contains(it) }
+        neighbours.forEach {
+            shortestPaths[it] = Math.min(shortestPaths[it]!!, shortestPaths[e]!! + 1)
+        }
+    }
+    return shortestPaths[g.end()]!!
+}
+
+data class Coord(val x: Int, val y: Int) {
+    fun neighbours() = listOf(copy(x = x+1), copy(x = x-1), copy(y = y+1), copy(y = y-1))
+}
+
+data class Grid(val lines: List<String>) {
+    fun height() = lines.size
+    fun width() = lines.first().length
+
+    fun cellAt(coord: Coord) = lines[coord.y][coord.x]
+
+    fun start() = findCell('S')
+    fun end() = findCell('E')
+
+    fun findCell(c: Char): Coord {
+        (0 until width()).map {x ->
+            (0 until height()).map { y ->
+                if (cellAt(Coord(x, y)) == c) return Coord(x,y)
+            }
+        }
+        throw RuntimeException("Couldn't find $c")
+    }
+
+    fun gridContains(c: Coord) = (0 until height()).contains(c.y) && (0 until width()).contains(c.x)
+
+    fun neighbours(c: Coord) : List<Coord> {
+        val heightAtC = heightAt(c)
+        return c.neighbours().filter { gridContains(it) }.filter { heightAt(it) <= heightAtC+1 }
+    }
+
+    private fun heightAt(c: Coord): Int {
+        return when (cellAt(c)) {
+            'S' -> 0
+            'E' -> 'z' - 'a'
+            else -> cellAt(c) - 'a'
+        }
+    }
+
 }
