54e7ebc2550d66148525c1db75db80b7a9b36936
Mike Hill <mikewhill@gmail.com>
2024-01-28 13:18:48 +0000

    mike - prep for day 12

---
+++ src/main/kotlin/day12/Main.kt
@@ -0,0 +1,21 @@
+package day12
+
+import java.io.File
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+    val puzzleInput = File("day12/puzzleInput.txt").readText()
+    println("answer1: " + answer(puzzleInput))
+    println("answer2: " + answer2(puzzleInput))
+
+}
+
+fun answer2(puzzleInput: String): Long {
+    return 0
+}
+
+fun answer(puzzleInput: String): Int {
+    return 0
+}
---
+++ src/test/kotlin/day12/Day12Test.kt
@@ -0,0 +1,22 @@
+package day12
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day12Test {
+    val exampleInput = """Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+
+    @Test
+    fun `check part 1 example`() {
+        Assertions.assertThat(answer(exampleInput)).isEqualTo(31)
+    }
+
+    @Test
+    fun `check part 2 example`() {
+        Assertions.assertThat(answer2(exampleInput)).isEqualTo(0)
+    }
+}
