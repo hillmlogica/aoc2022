4c5e152d66e7376169a05d687ef923c135abdc53
Mike Hill <mikewhill@gmail.com>
2024-02-05 06:23:13 +0000

    mike - day 14 prep

---
+++ src/test/kotlin/day14/Day14Test.kt
@@ -0,0 +1,19 @@
+package day14
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day14Test {
+    val exampleInput = """498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9"""
+
+    @Test
+    fun `check part 1 example`() {
+        Assertions.assertThat(answer(exampleInput)).isEqualTo(24)
+    }
+
+    @Test
+    fun `check part 2 example`() {
+        Assertions.assertThat(answer2(exampleInput)).isEqualTo(0)
+    }
+}
