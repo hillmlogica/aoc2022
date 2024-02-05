e79df06d59d6e5b363973043e1eb0830a2f640e4
Mike Hill <mikewhill@gmail.com>
2024-02-05 06:19:16 +0000

    mike - day 13

--- src/main/kotlin/day13/Main.kt
+++ src/main/kotlin/day13/Main.kt
@@ -10,9 +10,97 @@ fun main() {
 }
 
 fun answer2(puzzleInput: String): Int {
-    return 0
+    val sorted = (listOf(parse("[[2]]"), parse("[[6]]")) + puzzleInput.lines().filter { it.isNotBlank() }.map { parse(it) }).sortedWith { o1, o2 -> compare(o1, o2) }
+    return (sorted.indexOf(parse("[[2]]")) + 1) * (sorted.indexOf(parse("[[6]]")) + 1)
 }
 
 fun answer(puzzleInput: String): Int {
+    val pairs = puzzleInput.lines().filter { it.isNotBlank() }.chunked(2)
+    return pairs.map { Pair(parse(it[0]), parse(it[1])) }.mapIndexed { index, pair ->
+        if (compare(
+                pair.first,
+                pair.second
+            ) < 0
+        ) { println(index+1)
+            index + 1} else 0
+    }.sum()
+}
+
+//[1,[2,[3,[4,[5,6,7]]]],8,9]
+fun parse(line: String): Node {
+    return parseList(line, 0).first
+}
+
+fun parseList(line: String, index: Int): Pair<Node, Int> {
+    if (line[index] != '[') {
+        throw RuntimeException("Not a list!")
+    }
+
+    val myList = mutableListOf<Node>()
+    var i = index + 1
+    while (line[i] != ']') {
+        if (line[i] == '[') {
+            val (n, nextI) = parseList(line, i)
+            myList += n
+            i = nextI
+        } else if (line[i].isDigit()) {
+            val (n, nextI) = parseNumber(line, i)
+            myList += n
+            i = nextI
+        } else if (line[i] == ',') {
+            i++
+        }
+    }
+    return Pair(ListNode(myList), i + 1)
+}
+
+fun parseNumber(line: String, index: Int): Pair<Node, Int> {
+    if (!line[index].isDigit()) {
+        throw RuntimeException("Not a number")
+    }
+    var i = index
+    val listDigits = mutableListOf<Char>()
+    while (line[i].isDigit()) {
+        listDigits += line[i]
+        i++
+    }
+    return Pair(ValueNode(listDigits.joinToString("").toInt()), i)
+}
+
+
+
+fun compare(left: Node, right: Node): Int {
+    if (left is ValueNode && right is ValueNode) {
+        return left.compareTo(right)
+    }
+    if (left is ListNode && right is ListNode) {
+        return left.compareTo(right)
+    }
+    if (left is ValueNode) {
+        return compare(ListNode(listOf(left)), right)
+    }
+    if (right is ValueNode) {
+        return compare(left, ListNode(listOf(right)))
+    }
     return 0
+}
+
+interface Node
+
+data class ListNode(val nodes: List<Node>) : Node {
+    fun compareTo(right: ListNode): Int {
+        val comparisons = nodes.zip(right.nodes).map { compare(it.first, it.second) }
+        val firstNonZero = comparisons.firstOrNull() { it != 0 }
+        if (firstNonZero == null) {
+            return nodes.size - right.nodes.size
+        } else {
+            return firstNonZero
+        }
+    }
+}
+
+data class ValueNode(val value: Int) : Node {
+    fun compareTo(valueNode: ValueNode): Int {
+        return value - valueNode.value
+    }
 }
--- src/test/kotlin/day13/Day13Test.kt
+++ src/test/kotlin/day13/Day13Test.kt
@@ -30,11 +30,75 @@ class Day13Test {
 
     @Test
     fun `check part 1 example`() {
-        Assertions.assertThat(answer(exampleInput)).isEqualTo(31)
+        Assertions.assertThat(answer(exampleInput)).isEqualTo(13)
     }
 
     @Test
     fun `check part 2 example`() {
-        Assertions.assertThat(answer2(exampleInput)).isEqualTo(29)
+        Assertions.assertThat(answer2(exampleInput)).isEqualTo(140)
     }
+
+    @Test
+    fun `compare values`() {
+        Assertions.assertThat(compare(ValueNode(1), ValueNode(2))).isLessThan(0)
+        Assertions.assertThat(compare(ValueNode(1), ValueNode(1))).isEqualTo(0)
+        Assertions.assertThat(compare(ValueNode(2), ValueNode(1))).isGreaterThan(0)
+    }
+
+    @Test
+    fun `compare lists`() {
+        val firstList = ListNode(listOf(ValueNode(1), ValueNode(1), ValueNode(3), ValueNode(1), ValueNode(1)))
+        val secondList = ListNode(listOf(ValueNode(1), ValueNode(1), ValueNode(5), ValueNode(1), ValueNode(1)))
+        Assertions.assertThat(compare(firstList, secondList)).isLessThan(0)
+    }
+
+    @Test
+    fun `compare lists of different sizes`() {
+        val first = ListNode(listOf())
+        val second = ListNode(listOf(ValueNode(3)))
+        Assertions.assertThat(compare(first, second)).isLessThan(0)
+    }
+    @Test
+    fun `compare value to list`() {
+        val first = ListNode(listOf(ValueNode(2), ValueNode(3), ValueNode(4)))
+        val second = ValueNode(4)
+        Assertions.assertThat(compare(first, second)).isLessThan(0)
+    }
+
+    @Test
+    fun `compare lists of node lists`() {
+        val first = ListNode(listOf(ListNode(listOf(ValueNode(1))), ListNode(listOf(ValueNode(2), ValueNode(3), ValueNode(4)))))
+        val second = ListNode(listOf(ListNode(listOf(ValueNode(1))), ValueNode(4)))
+        Assertions.assertThat(compare(first, second)).isLessThan(0)
+    }
+    @Test
+    fun `list vs list of lists`() {
+        val first = ListNode(listOf(ValueNode(9)))
+        val second = ListNode(listOf(ListNode(listOf(ValueNode(8), ValueNode(7), ValueNode(6)))))
+        Assertions.assertThat(compare(first, second)).isGreaterThan(0)
+    }
+    @Test
+    fun `running out of elements on lhs`() {
+        //[[4,4],4,4] vs [[4,4],4,4,4]
+        val first = ListNode(listOf(ListNode(listOf(ValueNode(4), ValueNode(4))), ValueNode(4), ValueNode(4)))
+        val second = ListNode(listOf(ListNode(listOf(ValueNode(4), ValueNode(4))), ValueNode(4), ValueNode(4), ValueNode(4)))
+        Assertions.assertThat(compare(first, second)).isLessThan(0)
+    }
+    @Test
+    fun `empty lists`() {
+        //[[[]]] vs [[]]
+        val first = ListNode(listOf(ListNode(listOf(ListNode(listOf())))))
+        val second = ListNode(listOf(ListNode(listOf())))
+        Assertions.assertThat(compare(first, second)).isGreaterThan(0)
+    }
+
+    @Test
+    fun `parse a list`() {
+        Assertions.assertThat(parse("[]")).isEqualTo(ListNode(listOf()))
+        Assertions.assertThat(parse("[1]")).isEqualTo(ListNode(listOf(ValueNode(1))))
+        Assertions.assertThat(parse("[1,2]")).isEqualTo(ListNode(listOf(ValueNode(1), ValueNode(2))))
+        Assertions.assertThat(parse("[1,2,[]]")).isEqualTo(ListNode(listOf(ValueNode(1), ValueNode(2), ListNode(listOf()))))
+        Assertions.assertThat(parse("[10]")).isEqualTo(ListNode(listOf(ValueNode(10))))
+    }
+
 }
